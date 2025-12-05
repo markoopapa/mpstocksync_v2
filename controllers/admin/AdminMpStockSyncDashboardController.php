877fdfde483f65d9770f626c7666908d3ad9c8c5
markoopapa <markoopapa@ymail.com>
2025-12-05 08:56:23 +0200

    Update AdminMpStockSyncDashboardController.php

--- controllers/admin/AdminMpStockSyncDashboardController.php
+++ controllers/admin/AdminMpStockSyncDashboardController.php
@@ -1,4 +1,7 @@
 <?php
+// NINCS namespace!
+// NINCS use statement!
+
 class AdminMpStockSyncDashboardController extends ModuleAdminController
 {
     public function __construct()
@@ -13,13 +16,40 @@ class AdminMpStockSyncDashboardController extends ModuleAdminController
     {
         parent::initContent();
         
+        // Get module instance
         $module = Module::getInstanceByName('mpstocksync');
+        
+        // Get sync statistics
+        $stats = [];
+        if ($module) {
+            $stats = $module->getSyncStatistics();
+        }
         
         $this->context->smarty->assign([
             'dashboard_url' => $this->context->link->getAdminLink('AdminMpStockSyncDashboard'),
-            'module' => $module
+            'stats' => $stats,
+            'module' => $module,
+            'emag_configured' => Configuration::get('MP_EMAG_CLIENT_ID') && Configuration::get('MP_EMAG_CLIENT_SECRET'),
+            'trendyol_configured' => Configuration::get('MP_TRENDYOL_API_KEY') && Configuration::get('MP_TRENDYOL_API_SECRET')
         ]);
         
         $this->setTemplate('dashboard.tpl');
+    }
+    
+    public function initToolbar()
+    {
+        parent::initToolbar();
+        
+        $this->page_header_toolbar_btn['sync_emag'] = [
+            'href' => self::$currentIndex . '&sync_emag&token=' . $this->token,
+            'desc' => 'Sync eMAG',
+            'icon' => 'process-icon-refresh'
+        ];
+        
+        $this->page_header_toolbar_btn['sync_trendyol'] = [
+            'href' => self::$currentIndex . '&sync_trendyol&token=' . $this->token,
+            'desc' => 'Sync Trendyol',
+            'icon' => 'process-icon-refresh'
+        ];
     }
 }
